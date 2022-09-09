1aa5d3d01197f06f4a47f274649d051503d112e3
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2022-09-09 15:51:16 +0530

    Update LongestCommonPrefix1.java

--- LongestCommonPrefix1.java
+++ LongestCommonPrefix1.java
@@ -6,10 +6,10 @@ public class LongestCommonPrefix1{
     StringBuilder result = new StringBuilder();
     Arrays.sort(strs);
     char[] first = strs[0].toCharArray();
-    char[] lasr = strs[strs.length-1].toCharArray();
+    char[] last = strs[strs.length-1].toCharArray();
 
     for(int i=0;i<first.length;i++){
-        if(first[i]!=lasr[i]){
+        if(first[i]!=last[i]){
             break;
         }
         result.append(first[i]);
